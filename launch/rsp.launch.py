b67c46c5725c61bd60b062046191beee955d971a
Thua Bui <thuabui1632@gmail.com>
2025-12-09 12:25:53 +0700

    Refactor rsp.launch.py to use Command for xacro
    Updated launch file to use Command for xacro processing instead of direct import. Adjusted package path and added comments for clarity.

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,34 +1,33 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command # <--- Thêm Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
-import xacro
-
+# Không cần import xacro ở đây nữa
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # 1. Kiểm tra xem có dùng giờ giả lập không
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
+    # 2. Lấy đường dẫn file Xacro
+    pkg_path = os.path.join(get_package_share_directory('robot2t'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
     
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    # 3. Xử lý file Xacro bằng lệnh Command (Cách chuẩn ROS 2)
+    # Thay vì xacro.process_file, ta dùng Command để gọi lệnh xacro từ terminal ảo
+    robot_description_config = Command(['xacro ', xacro_file])
+    
+    # 4. Tạo node Robot State Publisher
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
     )
-
 
     # Launch!
     return LaunchDescription([
